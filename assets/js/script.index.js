317791b1df93202f99f586fb1eb19d8510e63b17
sosofria <sophiamarcondes.564@gmail.com>
2023-08-22 09:25:18 -0300

    feat: create news functions

--- assets/js/script.index.js
+++ assets/js/script.index.js
@@ -8,3 +8,148 @@ class User{
         this.cpf = cpf;
     }
 }
+
+class ListUser{
+    constructor(nome, email, data, cidade, telefone, cpf, idade, signo, cliente){
+        this.nome = nome;
+        this.email = email;
+        this.data = data;
+        this.cidade = cidade;
+        this.telefone = telefone;
+        this.cpf = cpf;
+        this.idade = idade;
+        this.signo = signo;
+        this.cliente = cliente;
+    }
+}
+
+function createUser(){
+
+}
+
+function showUsers(){
+
+}
+
+function formatedCellphone(cellphone) {
+    console.log("Passou pela funcao formatedCellphone()");
+
+    let cellphoneArray = cellphone.split("");
+    let cellphoneFormated = "(" + cellphoneArray[0] + cellphoneArray[1] + ")"
+        + " " + cellphoneArray[2] + cellphoneArray[3] + cellphoneArray[4]
+        + cellphoneArray[5] + cellphoneArray[6] + "-"
+        + cellphoneArray[7] + cellphoneArray[8]
+        + cellphoneArray[9] + cellphoneArray[10];
+    return cellphoneFormated;
+}
+
+function valida_cpf(cpf) {
+    console.log("Passou pela funcao valida_cpf()");
+
+    var numeros, digitos, soma, i, resultado, digitos_iguais;
+    digitos_iguais = 1;
+    if (cpf.length < 11)
+        return false;
+    for (i = 0; i < cpf.length - 1; i++)
+        if (cpf.charAt(i) != cpf.charAt(i + 1)) {
+            digitos_iguais = 0;
+            break;
+        }
+    if (!digitos_iguais) {
+        numeros = cpf.substring(0, 9);
+        digitos = cpf.substring(9);
+        soma = 0;
+        for (i = 10; i > 1; i--)
+            soma += numeros.charAt(10 - i) * i;
+        resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+        if (resultado != digitos.charAt(0))
+            return false;
+        numeros = cpf.substring(0, 10);
+        soma = 0;
+        for (i = 11; i > 1; i--)
+            soma += numeros.charAt(11 - i) * i;
+        resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+        if (resultado != digitos.charAt(1))
+            return false;
+        return true;
+    }
+    else
+        return false;
+}
+
+function isUserAlreadyRegistered(cpf){
+
+}
+
+function calculateAge(){
+
+}
+
+function dateinPTBR(date){
+
+}
+
+function getZodiacSign(){
+    let birthdate = new Date(this.birthdate);
+    let day = birthdate.getDate();
+    let month = birthdate.getMonth() + 1;
+    console.log("Passou pelo getSigno() da class User");
+
+    if ((month == 1 && day <= 20) || (month == 12 && day >= 22)) {
+        return "Capricórnio ♑";
+    } else if ((month == 1 && day >= 21) || (month == 2 && day <= 18)) {
+        return "Aquário ♒";
+    } else if ((month == 2 && day >= 19) || (month == 3 && day <= 20)) {
+        return "Peixes ♓";
+    } else if ((month == 3 && day >= 21) || (month == 4 && day <= 20)) {
+        return "Áries ♈";
+    } else if ((month == 4 && day >= 21) || (month == 5 && day <= 20)) {
+        return "Touro ♉";
+    } else if ((month == 5 && day >= 21) || (month == 6 && day <= 20)) {
+        return "Gêmeos ♊";
+    } else if ((month == 6 && day >= 22) || (month == 7 && day <= 22)) {
+        return "Câncer ♋";
+    } else if ((month == 7 && day >= 23) || (month == 8 && day <= 23)) {
+        return "Leão ♌";
+    } else if ((month == 8 && day >= 24) || (month == 9 && day <= 23)) {
+        return "Virgem ♍";
+    } else if ((month == 9 && day >= 24) || (month == 10 && day <= 23)) {
+        return "Libra ♎";
+    } else if ((month == 10 && day >= 24) || (month == 11 && day <= 22)) {
+        return "Escorpião ♏";
+    } else if ((month == 11 && day >= 23) || (month == 12 && day <= 21)) {
+        return "Sagitário ♐";
+    }
+}
+
+function isPossibleClient(){
+    
+}
+
+function isAnyInputEmpty(){
+
+}
+
+function showRegister(){
+
+}
+
+function clearInputs(){
+
+}
+
+function sendsuccessMsg(msg){
+
+}
+
+function sendErrorMsg(msg) {
+    console.log("Passou pela funcao sendErrorMsg()");
+
+    document.getElementById("error-msg").innerHTML = msg;
+    document.getElementById("error-msg").classList.remove("hidden");
+    setTimeout(function () {
+        document.getElementById("error-msg").classList.add("hidden");
+    }, 4000);
+}
+
+        
