406323e33442a545b0fac725087e4179179f19ff
sosofria <sophiamarcondes.564@gmail.com>
2023-08-22 08:25:55 -0300

    feat: create html, css and js

---
+++ assets/js/script.index.js
@@ -0,0 +1,10 @@
+class User{
+    constructor(nome, email, data, cidade, telefone, cpf){
+        this.nome = nome;
+        this.email = email;
+        this.data = data;
+        this.cidade = cidade;
+        this.telefone = telefone;
+        this.cpf = cpf;
+    }
+}
